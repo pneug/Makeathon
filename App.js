b63034fe8c76c1d13555226d1d9c48d952008379
阪本 <immr.shen@hotmail.com>
2022-09-30 19:37:23 +0200

    Import paper library

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
