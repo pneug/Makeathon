39e6ebdd5564f407e60b2473e5c78348e045f290
Jasmineqy0 <jasmineqy0@gmail.com>
2022-10-01 13:25:03 +0200

    minor bug fix

--- app/screens/CameraScreen.js
+++ app/screens/CameraScreen.js
@@ -1,28 +1,41 @@
-import { Camera } from "expo-camera";
-import React from "react";
-import { Button } from 'react-native';
+// import { Camera } from "expo-camera";
+// import React from "react";
+// import { Button } from 'react-native';
 
-import { LoadingView } from "./tf/LoadingView";
-import { ModelView } from "./tf/ModelView";
-import { useTensorFlowLoaded } from "./tf/useTensorFlow";
+// import { LoadingView } from "./tf/LoadingView";
+// import { ModelView } from "./tf/ModelView";
+// import { useTensorFlowLoaded } from "./tf/useTensorFlow";
+
+// function CameraScreen(props) {
+//     const isLoaded = useTensorFlowLoaded();
+//     const [permission, requestPermission] = Camera.useCameraPermissions();
+  
+//     if (!permission?.granted) {
+//       return (
+//         <LoadingView message="Camera permission is required to continue">
+//           <Button title="Grant permission" onPress={requestPermission} />
+//         </LoadingView>
+//       );
+//     }
+    
+//     if (!isLoaded) {
+//       return <LoadingView message="Loading TensorFlow" />;
+//     }
+  
+//     return <ModelView />;
+// }
+
+// export default CameraScreen;
+
+import React from 'react';
+import { Text, View } from 'react-native';
 
 function CameraScreen(props) {
-    const isLoaded = useTensorFlowLoaded();
-    const [permission, requestPermission] = Camera.useCameraPermissions();
-  
-    if (!permission?.granted) {
-      return (
-        <LoadingView message="Camera permission is required to continue">
-          <Button title="Grant permission" onPress={requestPermission} />
-        </LoadingView>
-      );
-    }
-    
-    if (!isLoaded) {
-      return <LoadingView message="Loading TensorFlow" />;
-    }
-  
-    return <ModelView />;
+    return (
+        <View>
+            <Text>Camera</Text>
+        </View>
+    );
 }
 
 export default CameraScreen;
