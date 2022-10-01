5cf8eb1173232b35208bd1a339fc69ac3b96e148
Yuanchun Shen <y.c.shen@tum.de>
2022-10-01 20:16:02 +0200

    Add custom theme

--- app/routes/TabNavigator.js
+++ app/routes/TabNavigator.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {NavigationContainer } from '@react-navigation/native';
 import {createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import WarningScreen from '../screens/WarningScreen';
+import SettingsScreen from '../screens/Settings';
 import HomeStack from './HomeStack';
-import { Card } from 'react-native-paper';
+import { Card, Appbar } from 'react-native-paper';
 
 
 function TabNavigator () {
@@ -20,11 +20,11 @@ function TabNavigator () {
                         tabBarIcon: ({color, size}) => (
                             <MaterialCommunityIcons name="home" color={color} size={size} />
                         )}} />
-                <Tab.Screen name="Warning"
-                    component={WarningScreen} 
+                <Tab.Screen name="Settings"
+                    component={SettingsScreen} 
                     options={{
                         tabBarIcon: ({color, size}) => (
-                            <MaterialCommunityIcons name="alert" color={color} size={size} />
+                            <MaterialCommunityIcons name="cog" color={color} size={size} />
                         )}} />
             </Tab.Navigator>
         </NavigationContainer>
---
+++ app/screens/Settings.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { Title, Appbar } from "react-native-paper";
+
+export default function SettingsScreen(props){
+    return (
+        <ScrollView>
+            
+        </ScrollView>
+    );
+}
