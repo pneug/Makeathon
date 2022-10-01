2cf54c5f14650ff13488aa838ad62691c8deb6c2
Yuanchun Shen <y.c.shen@tum.de>
2022-10-01 14:18:38 +0200

    Add bottom tabs

--- app/routes/HomeStack.js
+++ app/routes/HomeStack.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
+import {NavigationContainer } from '@react-navigation/native';
 import {createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../screens/HomeScreen';
 import CameraScreen from '../screens/CameraScreen'
 import WeatherScreen from '../screens/WeatherScreen';
@@ -22,8 +24,31 @@ const screens = {
     }
 }
 
-const HomeStack = createStackNavigator(screens)
+const Tab = createBottomTabNavigator();
 
-export default createAppContainer(HomeStack)
+const HomeStack = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName="Home">
+                <Tab.Screen name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="home" color={color} size={size} />
+                        )
+                    }} />
+                <Tab.Screen name="Warning" component={WarningScreen} 
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="alert" color={color} size={size} />
+                        )
+                    }} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
 
+// export default createAppContainer(HomeStack)
+export default HomeStack;
 
